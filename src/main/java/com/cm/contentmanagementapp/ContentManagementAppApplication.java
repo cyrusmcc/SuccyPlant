576a610aeb864dbe295fd4007062576c9ed91c90
cyrusmcc <cyrusmcc@hotmail.com>
2021-08-01 19:26:43 -0600

    Initial commit

---
+++ src/main/java/com/cm/contentmanagementapp/ContentManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.cm.contentmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cm/contentmanagementapp/ContentManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.contentmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
