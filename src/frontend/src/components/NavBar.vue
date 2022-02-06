517d0622e2c26ccd64b33b5d7ae7d824b22a6c32
cyrusmcc <61042997+cyrusmcc@users.noreply.github.com>
2022-02-06 00:36:24 -0700

    Add desktop navbar
    Refactor navbar component

--- src/frontend/src/components/NavBar.vue
+++ src/frontend/src/components/NavBar.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="navBarContainer">
+    <router-link to="/" class="navBarText">succyplant</router-link>
     <div
       class="modalBackground"
       v-if="!displayHamburger"
       @click.self="toggleHamburger()"
     ></div>
-    <div class="navBar">
+    <div
+      class="hamburgerContent"
+      v-if="!displayHamburger"
+      @click="toggleHamburger"
+    >
+      <slot></slot>
+    </div>
+    <div class="sideBarContent" v-show="displaySideBar">
+      <router-link
+        class="navLink sideBarLink"
+        @click="toggleSideBar"
+        :to="'/p/' + $root.currentUser.username"
+      >
+        Profile
+      </router-link>
+      <router-link
+        to="/settings"
+        class="navLink sideBarLink"
+        @click="toggleSideBar"
+      >
+        Settings
+      </router-link>
+      <router-link
+        to="/settings"
+        class="navLink sideBarLink"
+        @click="toggleSideBar"
+        @click.prevent="logOut"
+      >
+        Logout
+      </router-link>
+    </div>
+    <div class="mobileNavBar" v-if="navType == 'mobile'">
       <div
         class="hamburgerLineContainer"
         v-if="displayHamburger"
@@ -15,7 +47,6 @@
         <div class="hamburgerLine"></div>
         <div class="hamburgerLine"></div>
       </div>
-      <router-link to="/" id="navBarText">succyplant</router-link>
       <div
         class="closeHamburgerButton"
         v-if="!displayHamburger"
@@ -24,44 +55,72 @@
         +
       </div>
     </div>
-    <div
-      class="hamburgerContent"
-      v-if="!displayHamburger"
-      @click="toggleHamburger"
-    >
-      <slot></slot>
+    <div class="desktopNavBar" v-if="navType == 'desktop'">
+      <div class="navLinks">
+        <router-link to="/" class="navLink"> Home </router-link>
+        <router-link to="/plants" class="navLink"> Plants </router-link>
+      </div>
+      <profile-pic v-if="$root.currentUser" @click="toggleSideBar()">
+        <img alt="profile picture" src="../assets/imgs/userDark.svg" />
+      </profile-pic>
     </div>
   </div>
 </template>
 
 <script>
+import ProfilePic from "./ProfilePic.vue";
+
 export default {
-  name: "Hamburger",
+  name: "NavBar",
   data() {
     return {
       displayHamburger: true,
+      displaySideBar: false,
+      screenWidth: window.innerWidth,
+      navType: "desktop",
     };
+  },
+  components: {
+    ProfilePic,
   },
   methods: {
     toggleHamburger() {
       this.displayHamburger = !this.displayHamburger;
     },
+    toggleSideBar() {
+      this.displaySideBar = !this.displaySideBar;
+    },
+    setWindowWidth() {
+      this.screenWidth = window.innerWidth;
+      if (this.screenWidth < 500) {
+        this.navType = "mobile";
+      } else {
+        this.navType = "desktop";
+      }
+    },
+  },
+  mounted() {
+    this.setWindowWidth();
+    window.onresize = () => {
+      this.setWindowWidth();
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .navBarContainer {
+  position: relative;
   z-index: 4;
   width: 100%;
-  height: fit-content;
+  height: 45px;
   position: fixed;
   top: 0;
   max-height: 100%;
   background: $primaryLight;
 }
 
-.navBar {
+.mobileNav {
   position: relative;
   z-index: 5;
   height: 25px;
@@ -75,6 +134,61 @@ export default {
   align-content: center;
   padding: 10px 0 10px 10px;
   background: $primaryLight;
+}
+
+.desktopNavBar {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+}
+
+.navLinks {
+  display: flex;
+  flex-direction: row;
+  column-gap: 10px;
+  width: fit-content;
+}
+
+.sideBarLink {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 1.8rem;
+}
+
+.sideBarLink:hover {
+  border: 2px solid transparent;
+  background: $outline;
+}
+
+.sideBarContent {
+  z-index: 5px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  border: 1px solid $outline;
+  width: 150px;
+  height: 150px;
+  position: absolute;
+  bottom: -145px;
+  right: 0;
+  background: $primaryLight;
+  box-shadow: $shadowLight;
+}
+
+.navBarText {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: $amatic;
+  color: $primaryDark;
+  font-size: 1.5rem;
+  cursor: pointer;
+  border-bottom: 2px solid transparent;
 }
 
 .hamburgerLineContainer {
@@ -92,6 +206,7 @@ export default {
 }
 
 .closeHamburgerButton {
+  z-index: 5;
   position: absolute;
   top: -7px;
   left: 10px;
@@ -109,11 +224,17 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  padding: 15px;
+  padding: 60px 15px 15px 15px;
   row-gap: 20px;
   background: $primaryLight;
   height: fit-content;
   max-width: 100%;
   font-size: 1.2rem;
+}
+
+#userPicImgCont {
+  width: 2rem;
+  height: 2rem;
+  cursor: pointer;
 }
 </style>
