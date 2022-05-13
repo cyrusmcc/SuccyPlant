19e2d350dcb77e37dea6dd4f63d930bf74d21040
Cyrus McCormick <cyrusmcc@hotmail.com>
2022-05-12 23:03:02 -0600

    Add init post tag script

--- src/main/java/com/cm/contentmanagementapp/dbinit/test.py
+++ src/main/java/com/cm/contentmanagementapp/dbinit/test.py
@@ -1,13 +1,56 @@
-file = open("plants.txt", 'r', encoding='utf-8')
+import os
+import mysql.connector
 
-for line in file:
+dB = mysql.connector.connect(
+    host=os.environ.get('HOST'),
+    user=os.environ.get('USER'),
+    password=os.environ.get('PASSWORD'),
+    database=os.environ.get('DB')
+
+)
+
+dBCursor = dB.cursor()
+
+plantFile = open("plants.txt", 'r', encoding='utf-8')
+tagFile = open("tags.txt", 'r', encoding='utf-8')
+
+for tag in tagFile:
+    t = tag.split(",", 1)
+    value = t[0].strip()
+    category = t[1].strip()
+    sql = "INSERT INTO content_tag (category, value) VALUES (%s, %s)"
+    val = (value, category)
+    print(value, category)
+    dBCursor.execute(sql, val)
+
+dB.commit()
+
+plants = []
+for line in plantFile:
+    # split scientific name, plant parameters
     split = line.split(",", 1)
-    scienceName = split[0].strip()
-    scienceName = scienceName.title()
-    print(scienceName)
+
+    plant = {}
+    scienceName = split[0].strip().title()
+    genus = scienceName.split(" ", 1)[0]
+    plant["scienceName"] = scienceName
+    plant["genus"] = scienceName.split(" ", 1)[0]
+
+
     attributes = []
 
     # If plant has properties specified, else it just has scientific name
     if len(split) == 2:
         attributes = split[1].split(" -")
-        commonName = attributes[0].title()
+        plant["commonName"] = attributes[0].title()
+        plant["type"] = attributes[1].split("=", 1)[1]
+        plant["size"] = attributes[2].split("=", 1)[1]
+        plant["water"] = attributes[3].split("=", 1)[1]
+        plant["light"] = attributes[4].split("=", 1)[1]
+        plant["petsafe"] = attributes[5].split("=", 1)[1]
+        plant["difficulty"] = attributes[6].split("=", 1)[1].strip()
+
+    plants.append(plant)
+
+for plant in plants:
+    print(plant)
